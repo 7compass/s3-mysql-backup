62186fb48747cae4558ab2386c81aab253d84949
Nate Riffe <inkblot@movealong.org>
2013-08-21 20:33:18 -0500

    Make mail optional
    Omit mailing by leaving the mail_to key unset in the configuration file.

--- lib/s3_mysql_backup.rb
+++ lib/s3_mysql_backup.rb
@@ -61,6 +61,7 @@ class S3MysqlBackup
   end
 
   def mail_notification(filename)
+    unless config['mail_to'] return
     stats = File.stat(filename)
     subject = "sql backup: #{@db_name}: #{human_size(stats.size)}"
 
