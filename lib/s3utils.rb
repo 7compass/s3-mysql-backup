9b858617ab44d3c8aa5999dd61ea8db5ac83da93
Jeff Emminger <jemminger@gmail.com>
2013-09-10 14:14:43 -0400

    switch from aws-s3 gem to aws-sdk

--- lib/s3utils.rb
+++ lib/s3utils.rb
@@ -1,30 +1,26 @@
-require 'aws/s3'
+require 'aws-sdk'
 
 # wrapper for S3 operations
 #
 class S3Utils
 
-  def initialize(access_key_id, secret_access_key, bucket, server=nil)
-    @bucket = bucket
-    connect(access_key_id, secret_access_key, server)
-    ensure_bucket_exists
+  def initialize(access_key_id, secret_access_key, bucket_name, server=nil)
+    @bucket_name   = bucket_name
+    @s3_connection = connect(access_key_id, secret_access_key, server)
+    @s3_bucket     = get_or_create_bucket
     self
   end
 
-  def copy(from, to)
-    AWS::S3::S3Object.copy(File.basename(from), File.basename(to), @bucket)
-  end
-
   def store(file_path)
-    AWS::S3::S3Object.store(File.basename(file_path), open(file_path), @bucket)
+    @s3_bucket.objects.create(File.basename(file_path), open(file_path))
   end
 
   def delete(file_path)
-    AWS::S3::S3Object.delete(File.basename(file_path), @bucket)
+    @s3_bucket.objects[File.basename(file_path)].delete
   end
 
   def list
-    AWS::S3::Bucket.find(@bucket).objects.each do |obj|
+    @s3_bucket.objects.each do |obj|
       puts "#{obj.bucket.name}/#{obj.key}"
     end
   end
@@ -33,12 +29,12 @@ class S3Utils
 
   def connect(access_key_id, secret_access_key, server=nil)
     if server == nil
-      AWS::S3::Base.establish_connection!(
+      AWS::S3::new(
         :access_key_id     => access_key_id,
         :secret_access_key => secret_access_key
       )
     else
-      AWS::S3::Base.establish_connection!(
+      AWS::S3::new(
         :access_key_id     => access_key_id,
         :secret_access_key => secret_access_key,
         :server            => server
@@ -46,11 +42,12 @@ class S3Utils
     end
   end
 
-  def ensure_bucket_exists
-    AWS::S3::Bucket.find(@bucket)
-
-  rescue AWS::S3::NoSuchBucket
-    AWS::S3::Bucket.create(@bucket)
+  def get_or_create_bucket
+    if @s3_connection.buckets[@bucket_name].exists?
+      @s3_connection.buckets[@bucket_name]
+    else
+      @s3_connection.buckets.create(@bucket_name)
+    end
   end
 
 end
--- s3-mysql-backup.gemspec
+++ s3-mysql-backup.gemspec
@@ -1,23 +1,23 @@
 Gem::Specification.new do |s|
-  s.name         = 's3-mysql-backup'
-  s.version      = '1.2.2'
+  s.name         = "s3-mysql-backup"
+  s.version      = "2.0.0"
   s.date         = Date.today
   s.summary      = "Simple mysql backup to S3"
   s.description  = "A simple mysql backup to Amazon S3"
   s.authors      = ["Jeff Emminger"]
-  s.email        = 'jeff@7compass.com'
-  s.files        = Dir['lib/*'] << 'README.md'
-  s.homepage     = 'https://github.com/7compass/s3-mysql-backup'
+  s.email        = "jeff@7compass.com"
+  s.files        = Dir["lib/*"] << "README.md"
+  s.homepage     = "https://github.com/7compass/s3-mysql-backup"
   s.platform     = Gem::Platform::RUBY
-  s.require_path = '.'
-  s.require_paths << 'lib'
+  s.require_path = "."
+  s.require_paths << "lib"
 
   s.executables  = "s3-mysql-backup"
 
-  s.add_runtime_dependency('aws-s3', [">= 0"])
+  s.add_runtime_dependency("aws-sdk", [">= 0"])
 
-  s.add_development_dependency 'rspec', '~> 2.12'
-  s.add_development_dependency 'rr', '~> 1.0.5'
+  s.add_development_dependency "rspec", "~> 2.12"
+  s.add_development_dependency "rr", "~> 1.0.5"
 
   s.test_files  = Dir.glob("spec/**/*.rb")
 end
